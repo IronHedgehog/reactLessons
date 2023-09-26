37ba4c6886c8fbd6ee189c7192500bbc53b9ae83
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-09-26 20:32:12 +0300

    components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Profile from "./components/profile/profile/Profile";
 
 function App() {
   const array = [];
+  const message = "";
+  const title = "";
 
   return (
     // React.Fragment
     <>
+      <div>
+        <h1>Hello</h1>
+
+        {title && <p> {title}</p>}
+        {message.length > 0 && <p>кількість символів {message.length}</p>}
+      </div>
       <FriendsList />
 
       {array.length ? <Profile /> : null}
