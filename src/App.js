f3160de56cce092d04f680d8265398b7ee0f6354
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-11-07 20:36:35 +0200

    uncontroll

--- src/App.js
+++ src/App.js
@@ -1,74 +1,30 @@
 import React, { Component } from "react";
-import Filter from "./components/filter/Filter";
-import Form from "./components/form/Form";
-import Numberlist from "./components/numberlist/Numberlist";
-
+// ПРИКЛАД НЕКОНТРОЛЬОВАНОЇ ФОРМИ
 export default class App extends Component {
-  state = {
-    phones: [
-      { id: 1, name: "Artur", phone: "735-07-97" },
-      { id: 2, name: "Shos-nebyd", phone: "735-06-77" },
-      { id: 3, name: "Hto-nebyd", phone: "735-09-57" },
-      { id: 4, name: "Bird", phone: "735-08-17" },
-    ],
-    filter: "",
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const login = form.elements.login.value;
+    const password = form.elements.password.value;
+    this.props.onSubmit({ login, password });
+    form.reset();
   };
-
-  plusContact = (contact) => {
-    if (
-      this.state.phones.find(
-        (phone) => phone.name.toLowerCase() === contact.name.toLowerCase()
-      )
-    ) {
-      alert("Такий контакт вже є");
-    } else {
-      this.setState((prev) => ({
-        phones: [...prev.phones, contact],
-      }));
-    }
-  };
-
-  plusFilter = (e) => {
-    const { value } = e.target;
-    this.setState({ filter: value });
-    // ПРЯМЕ ОНОВЛЕННЯ СТЕЙТУ === ТАК РОБИТИ НІКОЛИ НЕ МОЖНА
-    // this.state.filter = "PETRO";
-  };
-
-  filterByName = () => {
-    const { phones, filter } = this.state;
-    const filerPhones = phones.filter((phone) => {
-      return phone.name.toLowerCase().includes(filter.toLowerCase());
-    });
-    return filerPhones;
-  };
-
-  deletePhone = (id) => {
-    // this.setState((prev) => ({
-    //  phones: prev.phones.filter((item) => item.id !== id);
-    // }));
-    this.setState((prevState) => ({
-      phones: prevState.phones.filter((item) => item.id !== id),
-    }));
-  };
-
   render() {
-    // const phones = [
-    //   { id: 1, name: "Artur", phone: "735-07-97" },
-    //   { id: 2, name: "Shos-nebyd", phone: "735-06-77" },
-    //   { id: 3, name: "Hto-nebyd", phone: "735-09-57" },
-    //   { id: 4, name: "Bird", phone: "735-08-17" },
-    // ];
-    // PROPS READ ONLY
-    // const { phones } = this.state;
-    const { plusContact, plusFilter, filterByName, deletePhone } = this;
-    const filterElements = filterByName();
+    const { onSubmitForm } = this;
     return (
-      <>
-        <Form plusContact={plusContact} />
-        <Filter plusFilter={plusFilter} />
-        <Numberlist data={filterElements} deletePhone={deletePhone} />
-      </>
+      <form onSubmit={onSubmitForm}>
+        <label>
+          E-mail
+          <input type="text" name="login" placeholder="login" />
+        </label>
+        <label>
+          password
+          <input type="password" name="password" placeholder="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
     );
   }
 }
+
+// onSubmit, onInput
