3b2408f96a0a7d3046840b1ca7e49cbfd8c41915
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-05-28 19:11:38 +0300

    practice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { filterReducer } from "./filterSlice";
 import { taskReducer } from "./slice";
-
+import { useDispatch } from "react-redux";
+useDispatch;
 // Створення загального стору в який ми передали загальний стейт та підключення девтулзів
 export const store = configureStore({
   reducer: {
