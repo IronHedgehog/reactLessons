b728f981688fb804dd644811580e2797f6aa07c7
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-06-25 21:01:10 +0300

    selectors

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,31 +1,31 @@
 import { Task } from "components/Task/Task";
 import { useSelector } from "react-redux";
-import { filtersValue } from "redux/constants";
+import { neededTask } from "redux/selectors";
 import css from "./TaskList.module.css";
 
-const getNeddedTasks = (tasks, filter) => {
-  switch (filter) {
-    case filtersValue.active:
-      return tasks.filter((task) => !task.completed);
+// const getNeddedTasks = (tasks, filter) => {
+//   switch (filter) {
+//     case filtersValue.active:
+//       return tasks.filter((task) => !task.completed);
 
-    case filtersValue.completed:
-      return tasks.filter((task) => task.completed);
+//     case filtersValue.completed:
+//       return tasks.filter((task) => task.completed);
 
-    default:
-      return tasks;
-  }
-};
+//     default:
+//       return tasks;
+//   }
+// };
 
 export const TaskList = () => {
   // useSelector - дозволяє підписатись на стейт
-  const tasks = useSelector((state) => state.tasks);
+  // const tasks = useSelector((state) => state.tasks);
 
-  const filter = useSelector((state) => state.filter.status);
+  // const filter = useSelector((state) => state.filter.status);
 
-  const neddedTasks = getNeddedTasks(tasks, filter);
+  const neededTasks = useSelector((state) => neededTask(state));
   return (
     <ul className={css.list}>
-      {neddedTasks.map((task) => (
+      {neededTasks?.map((task) => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,20 @@
 export const getTasks = (state) => state.tasks;
 export const getFilterStatus = (state) => state.filter.status;
 
+export const neededTask = (state) => {
+  const tasks = getTasks(state);
+  const filter = getFilterStatus(state);
+
+  switch (filter) {
+    case filter.active:
+      return tasks.filter((task) => !task.completed);
+
+    case filter.completed:
+      return tasks.filter((task) => task.completed);
+
+    default:
+      return tasks;
+  }
+};
+
 console.log(getFilterStatus);
