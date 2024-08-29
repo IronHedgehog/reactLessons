38902f5a1d38ea6114144a6fdea05bdb9224fd1e
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-08-29 20:13:17 +0300

    a

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import Modal from "../modal/Modal";
 
 const Header = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const modalOpenFunc = () => {
+    setOpenModal(true);
+  };
+
+  const modalCloseFunc = () => {
+    setOpenModal(false);
+  };
+
   const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <>
@@ -27,9 +39,38 @@ const Header = () => {
           {isLoggedIn === false ? (
             <></>
           ) : (
-            <li>
-              <NavLink to={"/tasks"}>tasks</NavLink>
-            </li>
+            <>
+              <li>
+                <NavLink to={"/tasks"}>tasks</NavLink>
+              </li>
+            </>
+          )}
+
+          <li>
+            <button onClick={modalOpenFunc}>Open modal</button>
+          </li>
+          {openModal && (
+            <Modal>
+              <div
+                style={{
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  width: "500px",
+                  height: "500px",
+                  zIndex: "1000",
+                  backgroundColor: "green",
+                }}
+              >
+                <button onClick={modalCloseFunc}>Close modal</button>
+                <h2>MODAL</h2>
+                <h2>MODAL</h2>
+                <h2>MODAL</h2>
+                <h2>MODAL</h2>
+                <h2>MODAL</h2>
+              </div>
+            </Modal>
           )}
         </ul>
       </nav>
